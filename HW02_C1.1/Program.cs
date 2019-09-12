2f663e8ebb5dfd07de3bc3de7ba6a9db097bad4a
kavitha7sundarram <kavitha7sundarram@gmail.com>
2019-09-12 15:08:09 -0500

    commit

---
+++ HW02_C1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW02_C1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 20;
+            while(num<=20 && num>=1)
+            {
+                Console.Write($"{ num} ");
+                num = num - 1;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
