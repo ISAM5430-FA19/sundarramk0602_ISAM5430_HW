cdd45533180bec7e21952da8c65de144cdde7104
kavitha7sundarram <kavitha7sundarram@gmail.com>
2019-09-12 13:41:22 -0500

    B1

--- HW02_B1/Properties/AssemblyInfo.cs
+++ HW02_B1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HW1A")]
+[assembly: AssemblyTitle("HW02_B1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HW1A")]
+[assembly: AssemblyProduct("HW02_B1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b0060ba8-d7f8-4971-b833-afb55277f949")]
+[assembly: Guid("397bb54b-024d-41ff-89d2-51073007fd16")]
 
 // Version information for an assembly consists of the following four values:
 //
